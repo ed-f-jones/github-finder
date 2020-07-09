964dc496e6803b0726091c02ff0fe456926fc7ca
unknown <ed.f.jones@gmail.com>
2020-07-08 23:31:42 -0400

    alert completed and installed react router

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import axios from'axios';
+import Alert from './components/layout/Alert';
 
 
 class App extends Component {
 
 state = {
   users: [],
-  loading: false
+  loading: false,
+  alert: null
 }
 
   async componentDidMount() {
@@ -36,14 +38,30 @@ state = {
     this.setState({users: res.data.items, loading: false});
   }
 
+  // clear users from state
+clearUsers = () => this.setState({users: [], loading: false});
+
+// set alert
+setAlert = (msg, type) => {
+  this.setState({ alert: {msg, type} });
+
+  setTimeout(() => {
+    this.setState({alert: null})
+  }, 3000);
+}
+
   render() {
-    const name= "John Doe";
+    
+    const {users, loading} = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers}/>
-        <Users loading={this.state.loading} users={this.state.users}/>
+          <Alert alert={this.state.alert}/>
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={
+            users.length > 0 ? true : false} setAlert={this.setAlert}/>
+
+        <Users loading={loading} users={users}/>
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -4,23 +4,34 @@ import PropTypes from 'prop-types';
 export class Search extends Component {
     state = {
         text: ''
-    }
+    };
 
 static propTypes= {
-    searchUsers: PropTypes.func.isRequired
-}
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
+};
 
 onChange = (e) => {
     this.setState({[e.target.name]: e.target.value});
-}
+};
 
 onSubmit =(e) => {
  e.preventDefault();
-  this.props. searchUsers(this.state.text);
+ if(this.state.text === '') {
+     this.props.setAlert('Please enter a value', 'light');
+ } else {
+    this.props. searchUsers(this.state.text);
   this.setState({text: '' });
-}
+ }
+  
+};
 
     render() {
+
+        const {showClear, clearUsers} = this.props;
+
         return (
             <div>
                 <form onSubmit={this.onSubmit} className="form">
@@ -30,9 +41,14 @@ onSubmit =(e) => {
 
                     <input type="submit" value="Search" className="btn btn-dark btn-block"/>
                 </form>
+                {showClear && (
+                    <button className="btn btn-light btn-block"
+                     onClick={clearUsers}>Clear</button>
+                ) }
+                
             </div>
-        )
-    }
-}
+        );
+    };
+};
 
 export default Search
