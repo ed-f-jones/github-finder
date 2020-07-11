fccb41ee0e140473786becf85780f355f7d7346b
unknown <ed.f.jones@gmail.com>
2020-07-11 09:00:09 -0400

    navbar and user page complete

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import {Link} from 'react-router-dom';
 
  const UserItem  = ({user: {login, avatar_url, html_url}}) => {
      
@@ -8,7 +9,9 @@ import PropTypes from "prop-types";
             <img src={avatar_url} alt="" className="round-img"style={{width: '60px'}}/>
                 <h1>{login}</h1>
                 <div>
-                    <a href={html_url} className="btn btn-dark btn=sm my-1">More</a>
+                    <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
+                        More
+                    </Link>
                 </div>
             </div>
      );
